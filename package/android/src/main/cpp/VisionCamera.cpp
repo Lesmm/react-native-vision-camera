7e0df2991045bd4bf21fa7d6eab607e0eae163d6
jun.shen <shenj6@xiaopeng.com>
2024-06-26 14:47:03 +0800

    add ndk 23.1.7779620 sanitizer supports, reference:
    0. https://zhuanlan.zhihu.com/p/691182407
    1. https://developer.android.com/ndk/guides/asan?hl=zh-cn
    2. https://cloud.tencent.com/developer/article/1832835
    3. https://github.com/google/sanitizers/wiki/AddressSanitizerExampleStackOutOfBounds
    4. https://baijiahao.baidu.com/s?id=1798018647006610445&wfr=spider&for=pc
    5. https://mp.weixin.qq.com/s?__biz=MzIwNTIwMzAzNg==&mid=2654165348&idx=1&sn=9e2e1e15cf685f1a8140a287a888cc86&chksm=8cf38a57bb840341d5c933d1cf8299705e30cd765c7a7e5339016774eee9c4444af2c71c46e3&scene=21#wechat_redirect

--- package/android/src/main/cpp/VisionCamera.cpp
+++ package/android/src/main/cpp/VisionCamera.cpp
@@ -7,6 +7,8 @@
 #include <fbjni/fbjni.h>
 #include <jni.h>
 
+//static int *p;
+
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, [] {
     vision::VisionCameraInstaller::registerNatives();
@@ -17,5 +19,47 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
     vision::JFrameProcessor::registerNatives();
     vision::JSharedArray::registerNatives();
 #endif
+
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera", "Sanitizer Address: Start Test ...");
+//
+//      // 栈缓冲区溢出
+//      int stack_array[100];
+//      stack_array[1] = 0;
+//      int b = stack_array[100];  // BOOM
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera", "Sanitizer Address: stack buffer overflow -> %d", b);
+//
+//      // 堆内存溢出
+//      int *array = new int[100];
+//      array[0] = 0;
+//      int res = array[100 + 32];  // BOOM
+//      delete [] array;
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera", "Sanitizer Address: heap memory overflow -> %d", res);
+//
+//      // 使用已释放的指针
+//      int *list = new int[1024];
+//      list[0] = 11;
+//      delete[] list;
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera",
+//                          "Sanitizer Address: UseAfterFree list[0]=%d, list[1024]=%d", list[0], list[1024]);
+//
+//      // 重复释放指针
+//      int *arr = new int[1024];
+//      arr[0] = 11;
+//      delete[] arr;
+//      delete[] arr;
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera", "Sanitizer Address: Double free arr[0]=%d", arr[0]);
+//
+//      // 作用域之外使用其指针
+//      {
+//          int a = 0;
+//          p = &a;
+//      }
+//      *p = 1111;
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera", "Sanitizer Address: UseAfterScope *p=%d", *p);
+//
+//      // 分配内存没被释放
+//      void *ptr = malloc(sizeof(int));
+//      __android_log_print(ANDROID_LOG_INFO, "VisionCamera", "Sanitizer Address: MemoryLeaks %p;", ptr);
+
   });
 }
